af12dc52b7f23bd8f59114563470ad4c54f549bb
Lucas-132 <lucas.pollet135@gmail.com>
2025-11-18 11:12:18 +0100

    created routes and html files

--- myServer.js
+++ myServer.js
@@ -2,8 +2,19 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     console.log("server created");
+    response.getHeader("content-type","text/html");
+
+    let file = "";
+    if (response.url === "/"){
+        file = "./index.html";
+    } else if (response.urll === "/result"){
+        file = "./result.html";
+    } else {
+        file = "./error.html"
+    }
 });
 
 server.listen(3002,"localhost", () => {
-    console.log("now listening port 3002");
+    console.log("now listening port 3002");    
 });
+
